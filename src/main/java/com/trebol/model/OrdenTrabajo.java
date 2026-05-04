ba94a105db71f1a7a9c38c5bd7aa4cd4b72186f7
L33B21 <s21120035@itsm-tlapa.edu.mx>
2026-05-03 18:44:06 -0600

    Fase 4: Agregado modelo OrdenTrabajo

---
+++ src/main/java/com/trebol/model/OrdenTrabajo.java
@@ -0,0 +1,117 @@
+package com.trebol.model;
+
+public class OrdenTrabajo {
+    
+    private int idOrden;
+    private int idCliente;
+    private int idVehiculo;
+    private String fechaIngreso;
+    private String fechaEntregaEstimada;
+    private String estatus; // ej: 'pendiente', 'en proceso', 'terminado', 'entregado'
+    private String diagnosticoTecnico;
+    private String descripcionProblema;
+    private double costoManoObra;
+    private double costoRefacciones;
+
+    // Constructor vacío
+    public OrdenTrabajo() {
+    }
+
+    // Constructor con parámetros
+    public OrdenTrabajo(int idOrden, int idCliente, int idVehiculo, String fechaIngreso, 
+                        String fechaEntregaEstimada, String estatus, String diagnosticoTecnico, 
+                        String descripcionProblema, double costoManoObra, double costoRefacciones) {
+        this.idOrden = idOrden;
+        this.idCliente = idCliente;
+        this.idVehiculo = idVehiculo;
+        this.fechaIngreso = fechaIngreso;
+        this.fechaEntregaEstimada = fechaEntregaEstimada;
+        this.estatus = estatus;
+        this.diagnosticoTecnico = diagnosticoTecnico;
+        this.descripcionProblema = descripcionProblema;
+        this.costoManoObra = costoManoObra;
+        this.costoRefacciones = costoRefacciones;
+    }
+
+    // --- GETTERS Y SETTERS ---
+
+    public int getIdOrden() {
+        return idOrden;
+    }
+
+    public void setIdOrden(int idOrden) {
+        this.idOrden = idOrden;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdVehiculo() {
+        return idVehiculo;
+    }
+
+    public void setIdVehiculo(int idVehiculo) {
+        this.idVehiculo = idVehiculo;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getFechaEntregaEstimada() {
+        return fechaEntregaEstimada;
+    }
+
+    public void setFechaEntregaEstimada(String fechaEntregaEstimada) {
+        this.fechaEntregaEstimada = fechaEntregaEstimada;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getDiagnosticoTecnico() {
+        return diagnosticoTecnico;
+    }
+
+    public void setDiagnosticoTecnico(String diagnosticoTecnico) {
+        this.diagnosticoTecnico = diagnosticoTecnico;
+    }
+
+    public String getDescripcionProblema() {
+        return descripcionProblema;
+    }
+
+    public void setDescripcionProblema(String descripcionProblema) {
+        this.descripcionProblema = descripcionProblema;
+    }
+
+    public double getCostoManoObra() {
+        return costoManoObra;
+    }
+
+    public void setCostoManoObra(double costoManoObra) {
+        this.costoManoObra = costoManoObra;
+    }
+
+    public double getCostoRefacciones() {
+        return costoRefacciones;
+    }
+
+    public void setCostoRefacciones(double costoRefacciones) {
+        this.costoRefacciones = costoRefacciones;
+    }
+}
